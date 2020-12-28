6639d8fda647c90041a8c0265cfb144c7859eea5
Fenglei Qi <fenglei.qi@uni.lu>
2020-12-28 08:50:45 +0800

    styles update in compilation.

--- src/style_cohesion_model.h
+++ src/style_cohesion_model.h
@@ -1,2 +1,2 @@
-#include "cohesion_model_sjkr2.h"
 #include "cohesion_model_sjkr.h"
+#include "cohesion_model_sjkr2.h"
--- src/version_liggghts.h
+++ src/version_liggghts.h
@@ -1,1 +1,1 @@
-#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.2.1, compiled 2020-12-24-15:42:34 by fenglei, git commit 38c1fb49cbcf4aaf4a2e4a305c6ab79bf745de1b"
+#define LIGGGHTS_VERSION "LIGGGHTS-PUBLIC 3.2.1, compiled 2020-12-25-20:39:55 by Fenglei, git commit 38c1fb49cbcf4aaf4a2e4a305c6ab79bf745de1b"
